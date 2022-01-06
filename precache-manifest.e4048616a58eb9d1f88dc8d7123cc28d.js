dd11ecb6b57583865c7face851ddfea6a3ee1eed
Jay Trevino <jat2211@columbia.edu>
2022-01-06 00:34:16 -0600

    Updates

---
+++ precache-manifest.e4048616a58eb9d1f88dc8d7123cc28d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7211d1e8b3eb5157eba435724aa44b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6a17c68235724b39e70a",
+    "url": "/home/static/css/main.7190c8e8.chunk.css"
+  },
+  {
+    "revision": "e67ed16403b8da2ba367",
+    "url": "/home/static/js/2.e53e0ff9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.e53e0ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a17c68235724b39e70a",
+    "url": "/home/static/js/main.9a99a0c5.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "993e7f6824798e2b4e8d914cedc8672b",
+    "url": "/home/static/media/jaytrevino.993e7f68.JPG"
+  },
+  {
+    "revision": "aa780798a6e87998d2999f095d140e96",
+    "url": "/home/static/media/resume.aa780798.pdf"
+  }
+]);
